681dab57c82293544444c17e2e245420fa4f3556
Ankit377 <westartencare@gmail.com>
2020-09-26 11:08:04 +0530

    Updates

---
+++ precache-manifest.0c4d3f53494e2727a67a0f99ba619c14.js
@@ -0,0 +1,390 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223aa94fa87a2d35d6b59961b9ccd867",
+    "url": "/reactonlineshop/index.html"
+  },
+  {
+    "revision": "d861bc030c86d7613c5c",
+    "url": "/reactonlineshop/static/css/9.fe76b842.chunk.css"
+  },
+  {
+    "revision": "bb9d02cfd8d170477adb",
+    "url": "/reactonlineshop/static/css/main.c31d9593.chunk.css"
+  },
+  {
+    "revision": "3ed4f16ab5f4e8250ac3",
+    "url": "/reactonlineshop/static/js/0.8c0e4fc3.chunk.js"
+  },
+  {
+    "revision": "a6dfcbfd54d6c4c08f28",
+    "url": "/reactonlineshop/static/js/1.b92b4c2a.chunk.js"
+  },
+  {
+    "revision": "3f36d0aa9c454957abfd",
+    "url": "/reactonlineshop/static/js/10.c6ebbade.chunk.js"
+  },
+  {
+    "revision": "250d257ebd33fc0564d5",
+    "url": "/reactonlineshop/static/js/11.90713ed9.chunk.js"
+  },
+  {
+    "revision": "f83c6a4e4d384bf785d8",
+    "url": "/reactonlineshop/static/js/12.5581e496.chunk.js"
+  },
+  {
+    "revision": "441ead232dcb57f07be1",
+    "url": "/reactonlineshop/static/js/13.93cacc27.chunk.js"
+  },
+  {
+    "revision": "ab18a2f2c17690e63914",
+    "url": "/reactonlineshop/static/js/14.ba4fa54a.chunk.js"
+  },
+  {
+    "revision": "c6d80c9fda163ea5268f",
+    "url": "/reactonlineshop/static/js/15.994dd2c8.chunk.js"
+  },
+  {
+    "revision": "ed22eabf2ff2cb2b6f9f",
+    "url": "/reactonlineshop/static/js/16.45ebe8d4.chunk.js"
+  },
+  {
+    "revision": "c1a0a2c1868463bb778a",
+    "url": "/reactonlineshop/static/js/17.fa1e4404.chunk.js"
+  },
+  {
+    "revision": "d02b272e85685036471c",
+    "url": "/reactonlineshop/static/js/18.4d23383c.chunk.js"
+  },
+  {
+    "revision": "133e37262ca456244fed",
+    "url": "/reactonlineshop/static/js/19.501e8829.chunk.js"
+  },
+  {
+    "revision": "4bd3bc31a62257403585",
+    "url": "/reactonlineshop/static/js/2.b50b3e1c.chunk.js"
+  },
+  {
+    "revision": "c9027d5190ea92171774",
+    "url": "/reactonlineshop/static/js/20.ef9ab86a.chunk.js"
+  },
+  {
+    "revision": "a2a0de387550f4cfb2ca",
+    "url": "/reactonlineshop/static/js/21.a869d64a.chunk.js"
+  },
+  {
+    "revision": "0a257e8f76e0ec143f6f",
+    "url": "/reactonlineshop/static/js/22.792f1208.chunk.js"
+  },
+  {
+    "revision": "866bc5799bc9e0786afd",
+    "url": "/reactonlineshop/static/js/23.5c1fb3bd.chunk.js"
+  },
+  {
+    "revision": "f82d6d37ea73a9b92340",
+    "url": "/reactonlineshop/static/js/24.f3a617d0.chunk.js"
+  },
+  {
+    "revision": "6ac6a6bfa095dbb08e83",
+    "url": "/reactonlineshop/static/js/25.fa61cfce.chunk.js"
+  },
+  {
+    "revision": "357598fe3dcc6cc48957",
+    "url": "/reactonlineshop/static/js/26.3354cbf5.chunk.js"
+  },
+  {
+    "revision": "1595550e9a0f569280fa",
+    "url": "/reactonlineshop/static/js/27.f4ba3924.chunk.js"
+  },
+  {
+    "revision": "97f438f0ce03220b5246",
+    "url": "/reactonlineshop/static/js/28.60aadc7b.chunk.js"
+  },
+  {
+    "revision": "cf70f3520ea1790e6a5c",
+    "url": "/reactonlineshop/static/js/29.f8a74ccd.chunk.js"
+  },
+  {
+    "revision": "a37beac5468135347eaf",
+    "url": "/reactonlineshop/static/js/3.eb51e477.chunk.js"
+  },
+  {
+    "revision": "417ac473d864cbe5ab67",
+    "url": "/reactonlineshop/static/js/30.3ce01ef5.chunk.js"
+  },
+  {
+    "revision": "92c5cb6ae722a9170959",
+    "url": "/reactonlineshop/static/js/31.eb1cd58e.chunk.js"
+  },
+  {
+    "revision": "fe1b8eaccce648b2ab1f",
+    "url": "/reactonlineshop/static/js/32.e3b5ec13.chunk.js"
+  },
+  {
+    "revision": "f51fce430a27d74c6ec5",
+    "url": "/reactonlineshop/static/js/33.13a5360b.chunk.js"
+  },
+  {
+    "revision": "b064c315350cde36ede5",
+    "url": "/reactonlineshop/static/js/34.0ca247a6.chunk.js"
+  },
+  {
+    "revision": "a07ecf3fb2d385a12379",
+    "url": "/reactonlineshop/static/js/35.17d646dc.chunk.js"
+  },
+  {
+    "revision": "4578eb0ba3c7ce929ef9",
+    "url": "/reactonlineshop/static/js/36.8397db6c.chunk.js"
+  },
+  {
+    "revision": "71fe838eae8023a4c09d",
+    "url": "/reactonlineshop/static/js/37.c6fa8616.chunk.js"
+  },
+  {
+    "revision": "f0b82301f7ab20f9a518",
+    "url": "/reactonlineshop/static/js/38.7c39a02d.chunk.js"
+  },
+  {
+    "revision": "13a05938743ced225035",
+    "url": "/reactonlineshop/static/js/39.2c0e7b76.chunk.js"
+  },
+  {
+    "revision": "b2ec8d3ea80e79ce859e",
+    "url": "/reactonlineshop/static/js/4.ae8c0f63.chunk.js"
+  },
+  {
+    "revision": "889d63174dde56b4f598",
+    "url": "/reactonlineshop/static/js/40.9cc15dce.chunk.js"
+  },
+  {
+    "revision": "f19a8f0d7c5c275847cc",
+    "url": "/reactonlineshop/static/js/41.7d96c99c.chunk.js"
+  },
+  {
+    "revision": "4c1d246ab79599e324d7",
+    "url": "/reactonlineshop/static/js/42.5b42dc1e.chunk.js"
+  },
+  {
+    "revision": "b65434f03fbdc7690fea",
+    "url": "/reactonlineshop/static/js/43.7a480a4f.chunk.js"
+  },
+  {
+    "revision": "012fc86292387e69dbc8",
+    "url": "/reactonlineshop/static/js/44.c44ef4d5.chunk.js"
+  },
+  {
+    "revision": "264541fd3044599b3ce2",
+    "url": "/reactonlineshop/static/js/45.53cd05f0.chunk.js"
+  },
+  {
+    "revision": "157669d99928dcbd28e3",
+    "url": "/reactonlineshop/static/js/46.acaf3db9.chunk.js"
+  },
+  {
+    "revision": "7e4ec1945394da7c5fa1",
+    "url": "/reactonlineshop/static/js/47.c935edfd.chunk.js"
+  },
+  {
+    "revision": "1e17195a75c058019373",
+    "url": "/reactonlineshop/static/js/48.5987b8a6.chunk.js"
+  },
+  {
+    "revision": "e3c243dbf28415d1553f",
+    "url": "/reactonlineshop/static/js/49.2979410f.chunk.js"
+  },
+  {
+    "revision": "a13cc92cc148892f1d51",
+    "url": "/reactonlineshop/static/js/5.de0b566a.chunk.js"
+  },
+  {
+    "revision": "ec9a302913598d329354",
+    "url": "/reactonlineshop/static/js/50.23e5fc05.chunk.js"
+  },
+  {
+    "revision": "e1801712d193f1edfbe3",
+    "url": "/reactonlineshop/static/js/51.665d6685.chunk.js"
+  },
+  {
+    "revision": "c7474c52428e76fedd5d",
+    "url": "/reactonlineshop/static/js/52.57e5e64a.chunk.js"
+  },
+  {
+    "revision": "f396f5ae4681edea8157",
+    "url": "/reactonlineshop/static/js/53.92cf9226.chunk.js"
+  },
+  {
+    "revision": "4c8816938260c42afd89",
+    "url": "/reactonlineshop/static/js/54.99511bd9.chunk.js"
+  },
+  {
+    "revision": "492f821af81f4b14f050",
+    "url": "/reactonlineshop/static/js/55.c364b1f5.chunk.js"
+  },
+  {
+    "revision": "8e240166c951bff74b40",
+    "url": "/reactonlineshop/static/js/56.4b89d7cb.chunk.js"
+  },
+  {
+    "revision": "f6f227c2c1e152bb8f05",
+    "url": "/reactonlineshop/static/js/57.3b5abf45.chunk.js"
+  },
+  {
+    "revision": "2c5438c3676958445deb",
+    "url": "/reactonlineshop/static/js/58.23712956.chunk.js"
+  },
+  {
+    "revision": "36dfd14183eb68a86599",
+    "url": "/reactonlineshop/static/js/59.fd325cca.chunk.js"
+  },
+  {
+    "revision": "50c2a9ad2617e5a23a8a",
+    "url": "/reactonlineshop/static/js/6.606b0f90.chunk.js"
+  },
+  {
+    "revision": "fc854597dade5e8020c5",
+    "url": "/reactonlineshop/static/js/60.cd03b4d7.chunk.js"
+  },
+  {
+    "revision": "85f674776aa9a7b97b87",
+    "url": "/reactonlineshop/static/js/61.993e7605.chunk.js"
+  },
+  {
+    "revision": "10be228498c484ce9126",
+    "url": "/reactonlineshop/static/js/62.f2896af4.chunk.js"
+  },
+  {
+    "revision": "d919f972dedd5bf4ff25",
+    "url": "/reactonlineshop/static/js/63.0947f01f.chunk.js"
+  },
+  {
+    "revision": "2f3ddf9a6a0b2369618b",
+    "url": "/reactonlineshop/static/js/64.5073abbf.chunk.js"
+  },
+  {
+    "revision": "4456cce9fc20c37e6cff",
+    "url": "/reactonlineshop/static/js/65.6427c476.chunk.js"
+  },
+  {
+    "revision": "1871e283f65cf72bc493",
+    "url": "/reactonlineshop/static/js/66.4157f6f0.chunk.js"
+  },
+  {
+    "revision": "3a6445ea5a0b9919c0c6",
+    "url": "/reactonlineshop/static/js/67.2dc6e3a4.chunk.js"
+  },
+  {
+    "revision": "d6f262bc0a5d44c07225",
+    "url": "/reactonlineshop/static/js/68.52748f22.chunk.js"
+  },
+  {
+    "revision": "717945d49aabef20ddd2",
+    "url": "/reactonlineshop/static/js/69.8705cca9.chunk.js"
+  },
+  {
+    "revision": "62169999fcd9df2e1427",
+    "url": "/reactonlineshop/static/js/70.2f7b07dd.chunk.js"
+  },
+  {
+    "revision": "ca3423c3ac7ca0d94569",
+    "url": "/reactonlineshop/static/js/71.dcf6374b.chunk.js"
+  },
+  {
+    "revision": "46e7a6b0b58ab59100a4",
+    "url": "/reactonlineshop/static/js/72.6f5bd67d.chunk.js"
+  },
+  {
+    "revision": "39d0355f6012d1eb182a",
+    "url": "/reactonlineshop/static/js/73.6db3741d.chunk.js"
+  },
+  {
+    "revision": "0cb22d7538188c1d0bcc",
+    "url": "/reactonlineshop/static/js/74.94e21b7f.chunk.js"
+  },
+  {
+    "revision": "31b60c7b111bd3bc9b98",
+    "url": "/reactonlineshop/static/js/75.bf2980cb.chunk.js"
+  },
+  {
+    "revision": "f54f39d2da7f94b591aa",
+    "url": "/reactonlineshop/static/js/76.d64e6a9f.chunk.js"
+  },
+  {
+    "revision": "80e167907322da0030ef",
+    "url": "/reactonlineshop/static/js/77.b3c34121.chunk.js"
+  },
+  {
+    "revision": "b018f30301e8b4d50fd7",
+    "url": "/reactonlineshop/static/js/78.d1a78aae.chunk.js"
+  },
+  {
+    "revision": "837ebd247717788b7360",
+    "url": "/reactonlineshop/static/js/79.18e0a4c0.chunk.js"
+  },
+  {
+    "revision": "cbb85cfaaefa57263206",
+    "url": "/reactonlineshop/static/js/80.e1b079bf.chunk.js"
+  },
+  {
+    "revision": "516e79e24c49d7ffa868",
+    "url": "/reactonlineshop/static/js/81.a19d5fa3.chunk.js"
+  },
+  {
+    "revision": "d861bc030c86d7613c5c",
+    "url": "/reactonlineshop/static/js/9.605fb7be.chunk.js"
+  },
+  {
+    "revision": "bb9d02cfd8d170477adb",
+    "url": "/reactonlineshop/static/js/main.0542b64d.chunk.js"
+  },
+  {
+    "revision": "c3466781ab2384e476c2",
+    "url": "/reactonlineshop/static/js/runtime-main.4fea2395.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/reactonlineshop/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/reactonlineshop/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/reactonlineshop/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "dedf26989fdd78c33cb9ae664a604d6c",
+    "url": "/reactonlineshop/static/media/Pe-icon-7-stroke.dedf2698.svg"
+  },
+  {
+    "revision": "32400f4e08932a94d8bfd2422702c446",
+    "url": "/reactonlineshop/static/media/fontawesome-webfont.32400f4e.eot"
+  },
+  {
+    "revision": "65e8f7dd6a65aa5812afa9050bfc720e",
+    "url": "/reactonlineshop/static/media/fontawesome-webfont.65e8f7dd.svg"
+  },
+  {
+    "revision": "a35720c2fed2c7f043bc7e4ffb45e073",
+    "url": "/reactonlineshop/static/media/fontawesome-webfont.a35720c2.woff"
+  },
+  {
+    "revision": "a3de2170e4e9df77161ea5d3f31b2668",
+    "url": "/reactonlineshop/static/media/fontawesome-webfont.a3de2170.ttf"
+  },
+  {
+    "revision": "db812d8a70a4e88e888744c1c9a27e89",
+    "url": "/reactonlineshop/static/media/fontawesome-webfont.db812d8a.woff2"
+  },
+  {
+    "revision": "1fbfd4bcffccb94e8e8a5ea70616b296",
+    "url": "/reactonlineshop/static/media/lg.1fbfd4bc.woff"
+  },
+  {
+    "revision": "2ec2cb2199d4d881e6a6ad86690f6add",
+    "url": "/reactonlineshop/static/media/lg.2ec2cb21.svg"
+  },
+  {
+    "revision": "f4292655f93dd12d9b8e4fc067ef2489",
+    "url": "/reactonlineshop/static/media/lg.f4292655.ttf"
+  }
+]);
